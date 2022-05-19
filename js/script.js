419d79264e60f3843154c5fa2cc1a450e418c5d3
Pavel Belousov <signorpao@gmail.com>
2022-05-19 11:57:57 +0300

    create navigation v.0

--- js/script.js
+++ js/script.js
@@ -1,3 +1,44 @@
 'use strict';
 
+// pc or touchscreen
+const isMobile = {
+  Android: function() {
+    return navigator.userAgent.match(/Android/i);
+  },
+  BlackBerry: function() {
+    return navigator.userAgent.match(/BlackBerry/i);
+  },
+  iOS: function() {
+    return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+  },
+  Opera: function() {
+    return navigator.userAgent.match(/Opera Mini/i);
+  },
+  Windows: function() {
+    return navigator.userAgent.match(/IEMobile/i) || navigator.userAgent.match(/WPDesktop/i);
+  },
+  any: function() {
+    return (
+      isMobile.Android() || 
+      isMobile.BlackBerry() || 
+      isMobile.iOS() || 
+      isMobile.Opera() || 
+      isMobile.Windows());
+  }
+};
 
+if (isMobile.any()) {
+  document.body.classList.add('touch');
+
+  let menuArrows = document.querySelectorAll('.menu-arrow');
+  if (menuArrows.length > 0) {
+    for (let index = 0; index < menuArrows.length; index++) {
+      const menuArrow = menuArrows[index];
+      menuArrow.addEventListener('click', (e) => {
+        menuArrow.parentElement.classList.toggle('active');
+      });
+    }
+  }
+} else {
+  document.body.classList.add('pc');
+}
